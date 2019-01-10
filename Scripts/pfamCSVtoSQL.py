a87397d997ac20470de3e2ff2b1cf8db98232b83
FrenchFroonz <39059882+FrenchFroonz@users.noreply.github.com>
2019-01-10 22:50:11 +0100

    Parsers

---
+++ Scripts/pfamCSVtoSQL.py
@@ -0,0 +1,35 @@
+import os, sys
+import operator
+
+
+
+#IN : pfam.csv
+
+
+
+infile = sys.argv[1]
+
+gene_table = open("Results/pfamSQL.sql", "w")
+#gene_table.write("gene_id\tlength\tstart\tstop\tstrand\tname\tsupercontig\n")
+
+with open(infile,'r') as gene_file:
+    
+	line = gene_file.readline()
+	line = gene_file.readline()
+
+	while line != "":
+
+		line = line.replace("'","prime")
+
+		LIST = line.split(";")
+		for i in range(0,3):
+			#if ( (i != 1) or (i !=2) or (i !=3) or (i !=6)) :
+			LIST[i]="'" + LIST[i] + "'"
+		gene_table.write("INSERT INTO pfam (PfamAcc, Nom, Description) VALUES (")
+		gene_table.write(",".join([LIST[0]] + LIST[1:3]) + ");\n")
+		#gene_table.write([LIST[0] + LIST[1] + [LIST[2] + LIST[3] + LIST[3] + LIST[3] + LIST[3] + LIST[3:9]) + ");\n")
+		line = gene_file.readline()
+
+	gene_file.close()
+	gene_table.close()
+
